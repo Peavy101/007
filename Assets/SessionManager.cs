ff3ee8672040136f917a44144499de68bd1d9810
Peavy101 <matthewhardy709@gmail.com>
2022-12-31 16:17:19 -0500

    Base game function works!
    Getting the game to start working versus a computer giving random feedback.

--- Assets/SessionManager.cs
+++ Assets/SessionManager.cs
@@ -14,12 +14,23 @@ public class SessionManager : MonoBehaviour
     int playerTwoAmmo = 0;
 
     public TextMeshProUGUI resultText;
+    public TextMeshProUGUI playerOneLivesText;
+    public TextMeshProUGUI playerOneAmmoText;
+    public TextMeshProUGUI playerTwoLivesText;
+    public TextMeshProUGUI playerTwoAmmoText;
     public Button blockbutton;
     public Button reloadButton;
     public Button shootButton;
 
+    bool playerOneHasAmmo = false;
+    bool playerTwoHasAmmo = false;
+
     void Start()
     {
+        playerOneLivesText.text = "Player 1 Lives: " + playerOneLives.ToString();
+        playerOneAmmoText.text = "Player 1 Ammo: " + playerOneAmmo.ToString();
+        playerTwoLivesText.text = "Player 2 Lives: " + playerTwoLives.ToString();
+        playerTwoAmmoText.text = "Player 2 Ammo: " + playerTwoAmmo.ToString();
         blockbutton.onClick.AddListener(Block);
         reloadButton.onClick.AddListener(Reload);
         shootButton.onClick.AddListener(Shoot);
@@ -28,7 +39,8 @@ public class SessionManager : MonoBehaviour
 
     void Update()
     {
-        
+        playerOneHasAmmoCheck();
+        playerTwoHasAmmoCheck();
     }
 
     void Block()
@@ -58,39 +70,126 @@ public class SessionManager : MonoBehaviour
         }
         else if(playerSelection == 1 && computerSelection == 2)
         {
+            playerTwoAmmo ++;
+            updateNumbers();
             return "Player 2 reloads";
         }
-        else if(playerSelection == 1 && computerSelection == 3)
+        else if(playerSelection == 1 && computerSelection == 3 && playerTwoHasAmmo)
         {
+            playerTwoAmmo --;
+            updateNumbers();
             return "Player 1 blocks the shot!";
         }
         else if(playerSelection == 2 && computerSelection == 1)
         {
+            playerOneAmmo ++;
+            updateNumbers();
             return "Player 1 reloads!";
         }
         else if(playerSelection == 2 && computerSelection == 2)
         {
+            playerOneAmmo ++;
+            playerTwoAmmo ++;
+            updateNumbers();
             return "Both players reload!";
         }
-        else if(playerSelection == 2 && computerSelection == 3)
+        else if(playerSelection == 2 && computerSelection == 3 && playerTwoHasAmmo)
         {
+            playerOneLives --;
+            playerTwoAmmo --;
+            updateNumbers();
             return "Player 1 is shot!";
         }
-        else if(playerSelection == 3 && computerSelection == 1)
+        else if(playerSelection == 3 && computerSelection == 1 && playerOneHasAmmo)
         {
+            playerOneAmmo --;
+            updateNumbers();
             return "Player 2 blocks!";
         }
-        else if(playerSelection == 3 && computerSelection == 2)
+        else if(playerSelection == 3 && computerSelection == 2 && playerOneHasAmmo)
         {
+            playerTwoLives --;
+            playerOneAmmo --;
+            updateNumbers();
             return "Player 2 is shot!";
         }
-        else if(playerSelection == 3 && computerSelection == 3)
+        else if(playerSelection == 3 && computerSelection == 3 && playerOneHasAmmo)
         {
+            playerOneAmmo --;
+            playerTwoAmmo --;
+            updateNumbers();
             return "Both players shoot!";
+        }
+        // below is for if they shoot with no ammo
+        else if(playerSelection == 1 && computerSelection == 3 && !playerTwoHasAmmo)
+        {
+            return "Player 2 has no ammo!";
+        }
+        else if(playerSelection == 2 && computerSelection == 3 && !playerTwoHasAmmo)
+        {
+            playerOneAmmo ++;
+            updateNumbers();
+            return "Player 1 reloads, player two has no ammo!";
+        }
+        else if(playerSelection == 3 && computerSelection == 1 && !playerOneHasAmmo)
+        {
+            return "Player 2 blocks, player 1 has no ammo!";
+        }
+        else if(playerSelection == 3 && computerSelection == 2 && !playerOneHasAmmo)
+        {
+            playerTwoAmmo ++;
+            updateNumbers();
+            return "Player 2 reloads, player 1 has no ammo!";
+        }
+        else if(playerSelection == 3 && computerSelection == 3 && !playerOneHasAmmo && !playerTwoHasAmmo)
+        {
+            return "Neither players have ammo!";
+        }
+        else if(playerSelection == 3 && computerSelection == 3 && !playerOneHasAmmo && playerTwoHasAmmo)
+        {
+            return "Player 1 is shot! And has no ammo!";
+        }
+        else if(playerSelection == 3 && computerSelection == 3 && playerOneHasAmmo && !playerTwoHasAmmo)
+        {
+            return "Player 2 is shot! And has no ammo!";
         }
         else
         {
-            return "idk how this happened";
+            return "wooopdeedoo!";
+        }
+
+
+    }
+
+    void updateNumbers()
+    {
+        playerOneLivesText.text = "Player 1 Lives: " + playerOneLives.ToString();
+        playerOneAmmoText.text = "Player 1 Ammo: " + playerOneAmmo.ToString();
+        playerTwoLivesText.text = "Player 2 Lives: " + playerTwoLives.ToString();
+        playerTwoAmmoText.text = "Player 2 Ammo: " + playerTwoAmmo.ToString();
+    }
+
+    void playerOneHasAmmoCheck()
+    {
+        if(playerOneAmmo > 0)
+        {
+            playerOneHasAmmo = true;
+        }
+        else if(playerOneAmmo == 0)
+        {
+            playerOneHasAmmo = false;
+        }
+    }
+    
+    void playerTwoHasAmmoCheck()
+    {
+        if(playerTwoAmmo > 0)
+        {
+            playerTwoHasAmmo = true;
+        }
+        else if(playerTwoAmmo == 0)
+        {
+            playerTwoHasAmmo = false;
         }
     }
 
