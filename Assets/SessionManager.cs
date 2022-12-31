3d21d160a12bcf536d0b9e32f4ed0035f2ae3fb1
Peavy101 <matthewhardy709@gmail.com>
2022-12-31 15:13:41 -0500

    Code change
    got inspo from chapt gpt from RPS game

--- Assets/SessionManager.cs
+++ Assets/SessionManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SessionManager : MonoBehaviour
 {
@@ -11,9 +13,16 @@ public class SessionManager : MonoBehaviour
     int playerOneAmmo = 0;
     int playerTwoAmmo = 0;
 
+    public TextMeshProUGUI resultText;
+    public Button blockbutton;
+    public Button reloadButton;
+    public Button shootButton;
+
     void Start()
     {
-        GameStart();
+        blockbutton.onClick.AddListener(Block);
+        reloadButton.onClick.AddListener(Reload);
+        shootButton.onClick.AddListener(Shoot);
     }
 
 
@@ -22,24 +31,67 @@ public class SessionManager : MonoBehaviour
         
     }
 
-
-    void GameStart()
+    void Block()
     {
-        while((playerOneLives > 0) && (playerTwoLives > 0))
+        int playerSelection = 1;
+        int computerSelection = Random.Range(1, 4);
+        resultText.text = DetermineWinner(playerSelection, computerSelection);
+    }
+    void Reload()
+    {
+        int playerSelection = 2;
+        int computerSelection = Random.Range(1, 4);
+        resultText.text = DetermineWinner(playerSelection, computerSelection);
+    }
+    void Shoot()
+    {
+        int playerSelection = 3;
+        int computerSelection = Random.Range(1, 4);
+        resultText.text = DetermineWinner(playerSelection, computerSelection);
+    }
+
+    string DetermineWinner(int playerSelection, int computerSelection)
+    {
+        if(playerSelection == 1 && computerSelection == 1)
         {
-            Debug.Log("Welcome to 007, press w to shoot, s to block, or a to reload");
+            return "Both block!";
         }
-    }
-
-
-    void GetInput()
-    {
-
-    }
-
-    void GetResults()
-    {
-
+        else if(playerSelection == 1 && computerSelection == 2)
+        {
+            return "Player 2 reloads";
+        }
+        else if(playerSelection == 1 && computerSelection == 3)
+        {
+            return "Player 1 blocks the shot!";
+        }
+        else if(playerSelection == 2 && computerSelection == 1)
+        {
+            return "Player 1 reloads!";
+        }
+        else if(playerSelection == 2 && computerSelection == 2)
+        {
+            return "Both players reload!";
+        }
+        else if(playerSelection == 2 && computerSelection == 3)
+        {
+            return "Player 1 is shot!";
+        }
+        else if(playerSelection == 3 && computerSelection == 1)
+        {
+            return "Player 2 blocks!";
+        }
+        else if(playerSelection == 3 && computerSelection == 2)
+        {
+            return "Player 2 is shot!";
+        }
+        else if(playerSelection == 3 && computerSelection == 3)
+        {
+            return "Both players shoot!";
+        }
+        else
+        {
+            return "idk how this happened";
+        }
     }
 
 }
